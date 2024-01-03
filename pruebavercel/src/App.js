10de75ecf1c499a4e95662096a457744257464ad
JessicaMarisolMaliza <jessicamarisol.maliza507@comunidadunir.net>
2024-01-02 23:45:19 -0500

    prueba vercerl

---
+++ pruebavercel/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          PRUEBA <code>src/App.js</code> PRUEBA.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ECUADOR
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
